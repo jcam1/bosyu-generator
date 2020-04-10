7e04c0c9da54507181fe4389b767b00ecb4fed94
Izuho Fujiwara <contact@fujishan.jp>
2020-04-10 15:25:42 +0900

    Updates

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 var backgroundPickr = new Pickr({
   el: ".background-color-pickr",
   theme: "classic",
-  default: "#333",
+  default: "#1E18C1",
   swatches: ["rgba(244, 67, 54, 1)", "rgba(233, 30, 99, 0.95)", "rgba(156, 39, 176, 0.9)", "rgba(103, 58, 183, 0.85)", "rgba(63, 81, 181, 0.8)", "rgba(33, 150, 243, 0.75)", "rgba(3, 169, 244, 0.7)", "rgba(0, 188, 212, 0.7)", "rgba(0, 150, 136, 0.75)", "rgba(76, 175, 80, 0.8)", "rgba(139, 195, 74, 0.85)", "rgba(205, 220, 57, 0.9)", "rgba(255, 235, 59, 0.95)", "rgba(255, 193, 7, 1)"],
   components: {
     preview: true,
@@ -18,7 +18,7 @@ var backgroundPickr = new Pickr({
 var fontPickr = new Pickr({
   el: ".font-color-pickr",
   theme: "classic",
-  default: "#939393",
+  default: "#DDE441",
   swatches: ["rgba(244, 67, 54, 1)", "rgba(233, 30, 99, 0.95)", "rgba(156, 39, 176, 0.9)", "rgba(103, 58, 183, 0.85)", "rgba(63, 81, 181, 0.8)", "rgba(33, 150, 243, 0.75)", "rgba(3, 169, 244, 0.7)", "rgba(0, 188, 212, 0.7)", "rgba(0, 150, 136, 0.75)", "rgba(76, 175, 80, 0.8)", "rgba(139, 195, 74, 0.85)", "rgba(205, 220, 57, 0.9)", "rgba(255, 235, 59, 0.95)", "rgba(255, 193, 7, 1)"],
   components: {
     preview: true,
@@ -32,11 +32,16 @@ var fontPickr = new Pickr({
 });
 var preview = document.getElementById("preview");
 var ctx = preview.getContext("2d");
-var width = 1025;
+var width = 1024;
 var height = 576;
 var title = "";
 var body = "";
-backgroundDraw(height, width, "#333");
+backgroundPickr.on("init", function (instance) {
+  backgroundDraw(height, width, instance.getColor().toRGBA());
+});
+fontPickr.on("init", function () {
+  updateText();
+});
 backgroundPickr.on("save", function (color) {
   backgroundDraw(height, width, color.toRGBA());
   updateText();
